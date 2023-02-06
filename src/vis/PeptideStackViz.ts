9b77579a34568f97a22c634cc0e87dfc079244cf
Harith Rathish <harith1996@gmail.com>
2023-02-06 16:02:50 +0100

    Added comments

--- src/vis/PeptideStackViz.ts
+++ src/vis/PeptideStackViz.ts
@@ -17,67 +17,70 @@ type Peptide = {
 };
 
 export default class PeptideStackVis {
-	proteins: Array<Protein>;
-	peptides: Array<Peptide>;
-	mainContainerId: string;
-	mainSvg: d3.Selection<BaseType, unknown, HTMLElement, any>;
-	svgWidth: number;
-	svgHeight: number;
-	tickLimit: number;
-	tickSize: number;
-	padding: number;
-	axisGap: number
+	proteins: Array<Protein>;		
+	peptides: Array<Peptide>;										//Datasets
+	mainSvgId: string;												//id of svg in parent
+	mainSvg: d3.Selection<BaseType, unknown, HTMLElement, any>;		//svg element
+	svgWidth: number;												//width of plot
+	svgHeight: number;												//height of plot
+	padding: number;												//horizontal padding
+	tickLimit: number;												//maximum number of ticks on one axis
+	tickGap: number;												//gap between each tick
+	axisGap: number													//vertical gap between split axes
 
 	constructor(
 		proteins: Array<Protein>,
 		peptides: Array<Peptide>,
-		mainContainerId: string,
+		mainSvgId: string,
 		width = 1000,
-		height = 2000
+			height = 2000
 	) {
 		this.proteins = proteins;
 		this.peptides = peptides;
-		this.mainContainerId = mainContainerId;
+		this.mainSvgId = mainSvgId;
 		this.svgWidth = width;
 		this.svgHeight = height;
-		this.tickSize = 20;
+		this.tickGap = 20;
 		this.padding = 100;
 		this.axisGap = 300;
-		this.tickLimit = Math.ceil(this.svgWidth / this.tickSize);
+		this.tickLimit = Math.ceil(this.svgWidth / this.tickGap);
 		this.mainSvg = d3
-			.select(this.mainContainerId)
+			.select(this.mainSvgId)
 			.attr("width", this.svgWidth)
 			.attr("height", this.svgHeight);
 	}
 
+	/**
+	 * Builds axes out of amino acid sequence of the given protein
+	 * @param protein Entry name of the protein
+	 */
 	buildSplitAxes(protein: string) {
 		let prot = this.proteins.find((p: Protein) => {
 			return p.Entry === protein;
 		});
 		let sequenceString = prot?.Sequence || "";
-		let sequenceIndices = Array.from(Array(sequenceString.length).keys());
-		let stringifiedIndices = sequenceIndices.map((n) => "" + n);
-		let number_of_axes = Math.ceil(sequenceString.length / this.tickLimit);
+		let sequenceIndices = Array.from(Array(sequenceString.length).keys());	//convert "ASOFUBNQOUFBEGQEOGBU..." => [0,1,2,3,4,5,6,7, ...]
+		let stringifiedIndices = sequenceIndices.map((n) => "" + n);			//converts [0,1,2,3,...] => ["0", "1", "2", "3", ...] to use with d3.scalePoint()
+		let number_of_axes = Math.ceil(sequenceString.length / this.tickLimit); 
 		let axes = [];
-
 		if (stringifiedIndices.length > 1) {
 			Array.from(Array(number_of_axes).keys()).forEach((i) => {
 				let axisDomain = stringifiedIndices.slice(
 					i * this.tickLimit,
 					(i + 1) * this.tickLimit
 				);
-				let isFullWidth = axisDomain.length === this.tickLimit;
+				let isFullWidth = axisDomain.length === this.tickLimit;			//does the axis span the full plot's width?
 				let seqScale = d3
 					.scalePoint()
 					.domain(axisDomain)
-					.range([this.padding, axisDomain.length * this.tickSize - (isFullWidth ? this.padding : 0)]);
+					.range([this.padding, axisDomain.length * this.tickGap - (isFullWidth ? this.padding : 0)]);		//add padding only if axis is full width
 				let seqAxis = d3.axisBottom(seqScale).tickFormat((d, t) => {
 					return sequenceString[parseInt(d)];
 				});
 				this.mainSvg
 					.append("g")
 					.call(seqAxis)
-					.attr("transform", `translate(0,${i * this.axisGap})`);
+					.attr("transform", `translate(0,${i * this.axisGap})`);	
 				axes.push(seqAxis);
 			});
 		}
